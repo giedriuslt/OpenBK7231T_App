9963939929293061d800700b2e0feca251ab9564
giedriuslt <123905703+giedriuslt@users.noreply.github.com>
2024-03-30 20:04:38 +0200

    Update drv_freeze.c

--- src/driver/drv_freeze.c
+++ src/driver/drv_freeze.c
@@ -1,14 +1,18 @@
 // freeze
 #include "drv_local.h"
+static int tmp_div = 0;
+static int tmp_divi =10;
+static int tmp_result =1; 
 
 void Freeze_OnEverySecond() {
 	while (1) {
 		// freeze
+		tmp_result = tmp_divi/tmp_div;
 	}
 }
 void Freeze_RunFrame() {
 	while (1) {
-		// freeze
+		tmp_result = tmp_divi/tmp_div;
 	}
 }
 void Freeze_Init() {
