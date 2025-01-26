cddfd0ac560f7eed8991b9e0296660bc76924188
giedriuslt <123905703+giedriuslt@users.noreply.github.com>
2025-01-26 20:12:42 +0200

    Update cmd_script.c

--- src/cmnds/cmd_script.c
+++ src/cmnds/cmd_script.c
@@ -302,7 +302,7 @@ scriptFile_t *SVM_RegisterFile(const char *fname) {
 	// cast from byte* to char*
 	if (!strcmp(fname, "@startup")) {
 		bk_printf("before dup cfg\n");
-		char * cm_ = malloc(strlen(CFG_GetShortStartupCommand()+4);
+		char * cm_ = malloc(strlen(CFG_GetShortStartupCommand()+4));
 		strcpy(cm_, CFG_GetShortStartupCommand());
 		r->data = cm_;
 		bk_printf("after dup cfg\n");
