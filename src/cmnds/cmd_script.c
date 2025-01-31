a228c425fe70a4a88c1107e6cf3ba119bdaa3deb
giedriuslt <123905703+giedriuslt@users.noreply.github.com>
2025-01-31 21:09:31 +0200

    Update cmd_script.c

--- src/cmnds/cmd_script.c
+++ src/cmnds/cmd_script.c
@@ -371,6 +371,11 @@ void SVM_RunThread(scriptInstance_t *t, int maxLoops) {
 	int loop = 0;
 	const char *start, *end;
 	int len, p;
+	
+	if(g_scrBuffer == NULL) {
+		g_scrBufferSize = 256;
+		g_scrBuffer = malloc(g_scrBufferSize + 1);
+	}
 
 	while(1) {
 		loop++;
@@ -423,6 +428,7 @@ void SVM_RunThread(scriptInstance_t *t, int maxLoops) {
 					g_scrBufferSize = len + 256;
 					bk_printf("before realloc\n");
 					g_scrBuffer = (char*)realloc(g_scrBuffer, g_scrBufferSize+1);
+					bk_printf("after realloc\n");
 				}
 				if (g_scrBuffer == NULL) {
 					return;
@@ -453,10 +459,6 @@ void SVM_RunThreads(int deltaMS) {
 	c_run = 0;
 	svm_deltaMS = deltaMS;
 
-	if(g_scrBuffer == NULL) {
-		g_scrBufferSize = 256;
-		g_scrBuffer = malloc(g_scrBufferSize + 1);
-	}
 
 	g_activeThread = g_scriptThreads;
 	while(g_activeThread) {
