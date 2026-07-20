29e3dbf77f1e62589a0120479e3b19aa2514d1ae
giedriuslt <123905703+giedriuslt@users.noreply.github.com>
2026-07-20 23:14:25 +0300

    try retry 2

--- src/httpserver/new_http.c
+++ src/httpserver/new_http.c
@@ -686,7 +686,7 @@ int postany(http_request_t *request, const char *str, int len)
         if (i < 0) {
             // Handle error (e.g., EINTR means interrupted, we can retry)
 			int ge = errno;
-            if (ge == EINTR) continue;
+            if (ge == EINTR || ge == EAGAIN || ge == EWOULDBLOCK) continue;
 			
 			ADDLOG_ERROR(LOG_FEATURE_HTTP, "postany fail %i errno %i fd %i", i, ge, request->fd);
             return -1; 
