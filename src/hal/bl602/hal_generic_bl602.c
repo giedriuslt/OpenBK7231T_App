4a5445c6a7f377190910fdb17fa797a47b3bd9a6
giedriuslt <123905703+giedriuslt@users.noreply.github.com>
2026-07-28 18:58:30 +0300

    BL602: dump FreeRTOS task list on watchdog timeout before reboot
    Switch the BL602 watchdog from reset mode to interrupt mode and hook
    TIMER_WDT_IRQn. On a watchdog bite the ISR re-arms the WDT in reset
    mode (2s backstop), prints the interrupted task name, the stuck PC
    (mepc), free heap and a vTaskList() dump over UART, then reboots.
    
    Not covered: hangs with global interrupts disabled - the WDT interrupt
    never fires and neither does the reset in that case.

--- src/hal/bl602/hal_generic_bl602.c
+++ src/hal/bl602/hal_generic_bl602.c
@@ -6,6 +6,12 @@
 #include <hal_sys.h>
 #include <bl_wdt.h>
 #include <bl_timer.h>
+#if PLATFORM_BL602
+#include <bl602_timer.h>
+#include <bl_irq.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#endif
 #else
 #include "bflb_common.h"
 #include "bflb_wdg.h"
@@ -25,10 +31,44 @@ void HAL_Delay_us(int delay)
 	bl_timer_delay_us(delay);
 }
 
+#if PLATFORM_BL602 && !PLATFORM_BL_NEW
+// Watchdog fires as an interrupt (not a reset), so we can dump diagnostics
+// before rebooting. The ISR first re-arms the watchdog in reset mode as a
+// backstop in case the dump itself hangs. Not covered: hangs with global
+// interrupts disabled - the interrupt never fires and neither does the reset.
+static void wdt_diag_irq(void)
+{
+	uint32_t tmpVal;
+	uint32_t mepc;
+	static char taskListBuf[1024];
+
+	// re-arm in RESET mode (~2s backstop); WDT clock is 32k/32 = ~1ms/tick
+	WDT_ResetCounterValue();
+	WDT_SetCompValue(2000);
+	WDT_IntMask(WDT_INT, MASK);
+	WDT_ENABLE_ACCESS();
+	tmpVal = BL_RD_REG(TIMER_BASE, TIMER_WICR);
+	BL_WR_REG(TIMER_BASE, TIMER_WICR, BL_SET_REG_BIT(tmpVal, TIMER_WICLR));
+
+	__asm volatile("csrr %0, mepc" : "=r"(mepc));
+	printf("\r\n!!! WDT bite - no feed for 3s\r\n");
+	printf("interrupted task: %s, stuck PC (mepc)=0x%08lx\r\n",
+		pcTaskGetName(NULL), (unsigned long)mepc);
+	printf("free heap: %u\r\n", (unsigned)xPortGetFreeHeapSize());
+	vTaskList(taskListBuf);
+	printf("Name\t\tState\tPrio\tStackHWM\tNum\r\n%s\r\n", taskListBuf);
+
+	hal_reboot();
+}
+#endif
+
 void HAL_Configure_WDT()
 {
 #if !PLATFORM_BL_NEW
-	bl_wdt_init(4000);
+	bl_wdt_init(3000);
+	bl_irq_register(TIMER_WDT_IRQn, wdt_diag_irq);
+	bl_irq_enable(TIMER_WDT_IRQn);
+	WDT_IntMask(WDT_INT, UNMASK);
 #else
 	struct bflb_wdg_config_s wdg_cfg;
 	wdg_cfg.clock_source = WDG_CLKSRC_32K;
